eb2379391bdc7cc10e05b905f091451563bf61a3
Alex <opetrovskyi423@gmail.com>
2024-02-13 11:19:26 +0100

    env add

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import { sequelize } from './services/db.js';
 
 dotenv.config();
 const app = express();
-const PORT = 3005;
+// const PORT = 3005;
 
 sequelize
   .authenticate()
@@ -18,9 +18,11 @@ sequelize
   })
 
 
-app.use(cors());
+app.use(cors({
+  origin:process.env.CLIENT_ORIGIN,
+}));
 app.use('/', express.json(), todoRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT} 🚀🚀🚀`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT} 🚀🚀🚀`);
 });
--- src/services/db.js
+++ src/services/db.js
@@ -3,19 +3,18 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const sequelize = new Sequelize(
-  // process.env.DB_HOST2,
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    port: process.env.PORT,
+    port: process.env.PORT_DB,
     dialect: 'postgres',
     logging: false,
     ssl: true,
     dialectOptions: {
       ssl: {
-        require: true, // Вимагати захищене з'єднання
+        require: true, 
       },
     },
   }
